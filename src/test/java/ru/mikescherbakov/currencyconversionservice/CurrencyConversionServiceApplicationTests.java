c563d21c8718df60655ecfce60c28c90cbafbc99
mike <scherbakov.mike@gmail.com>
2022-08-18 11:28:35 +0300

    configured exchange and conversion services

---
+++ src/test/java/ru/mikescherbakov/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikescherbakov.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
