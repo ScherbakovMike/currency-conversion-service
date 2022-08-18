d4804c23b850b63ae389f2a51d7002b26273218f
mike <scherbakov.mike@gmail.com>
2022-08-18 15:19:48 +0300

    exchange between microservices was implemented by OpenFeign

--- src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package ru.mikescherbakov.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("ru.mikescherbakov.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,14 @@
+package ru.mikescherbakov.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversionBean getConversion(
+            @PathVariable("from") String from,
+            @PathVariable("to") String to
+    );
+}
