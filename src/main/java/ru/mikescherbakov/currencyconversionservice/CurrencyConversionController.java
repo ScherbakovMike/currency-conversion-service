3589b81508832e94e9b1f53601d0bb4c9c49fcc4
mike <scherbakov.mike@gmail.com>
2022-08-19 14:47:09 +0300

    implemented ribbon client-side balancing

--- src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package ru.mikescherbakov.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,22 +9,18 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyConversionController {
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
 
-    private final CurrencyExchangeServiceProxy proxy;
-
-    public CurrencyConversionController(CurrencyExchangeServiceProxy proxy) {
-        this.proxy = proxy;
-    }
-
+    //mapping for currency-converter-feign service
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversionBean convertCurrency(
-            @PathVariable String from,
-            @PathVariable String to,
-            @PathVariable BigDecimal quantity
-    ) {
-        //calling the currency-exchange-service
-        var response = proxy.getConversion(from, to);
-        //creating a new response bean and getting the response back and taking it into Bean
+    //where {from} and {to} represents the column
+//returns a bean
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+//creating a new response bean
+//getting the response back and taking it into Bean
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 }
+
--- src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,14 +1,17 @@
 package ru.mikescherbakov.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//Enabling feign
+@FeignClient(name = "currency-exchange-service")
+//enabling ribbon
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    CurrencyConversionBean getConversion(
-            @PathVariable("from") String from,
-            @PathVariable("to") String to
-    );
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")        //where {from} and {to} are path variable
+    CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to); //from map to USD and to map to INR
 }
+
