7a4de3add7a912ceea76fd5ff514542b13acd641
mike <scherbakov.mike@gmail.com>
2022-08-20 11:44:11 +0300

    implemented Zuul gateway server

--- src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/ru/mikescherbakov/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,17 +1,16 @@
 package ru.mikescherbakov.currencyconversionservice;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
 //Enabling feign
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 //enabling ribbon
-@RibbonClient(name = "currency-exchange-service")
+//@RibbonClient(name = "netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeServiceProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")        //where {from} and {to} are path variable
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")        //where {from} and {to} are path variable
     CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to); //from map to USD and to map to INR
 }
 
